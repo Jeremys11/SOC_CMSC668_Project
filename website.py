5795b465dc358daa1ec2a5d8853b21c61f684f3b
Jeremys11 <jeremysingh116@gmail.com>
2021-12-05 11:46:45 -0500

    Website UI
    Added Login and Register for Companies and Employees
    
    Survey in style of OSMI in json form
    
    Website is made in Flask
    
    TODO:
    Integrate ML Model
    
    Get Data to and from Database to User/Company Homepages

--- website.py
+++ website.py
@@ -1,34 +1,59 @@
-from flask import Flask
-#import flask_login
-from flask import render_template
-#from tinydb import TinyDB, Query
-#from database import DatabaseQueries
+from flask import Flask, render_template, redirect, url_for, request
+import json
+
+
+#user_database = {"Jeremy":"pass","Bob":"corn"}
 
 ##Definition to run this file
 app = Flask(__name__)
 
 ## Homepage
+#Button to login goes to login.html
+#Button to register goes to register.html
 @app.route("/")
 def home():
-    #Button to login goes to login.html
-    #Button to register goes to register.html
     return render_template("homepage.html")
 
 ## URL with /login at end
 ## "Login Page"
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    #Views login API
-    #Test login message -- acces {{login_message}} in HTML file
-    login_message = "Login Message Test"
-    return render_template('login.html', login_message=login_message)
+
+    openfile = open('users.json')
+    user_database = json.load(openfile)
+    openfile.close()
+
+    error = None
+    if request.method == 'POST':
+        if user_database[request.form['username']] != request.form['password']:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template("error.html")
+        else:
+            return redirect(url_for('userhome', username=request.form['username']))
+
+    return render_template('login.html', error=error)
 
 #"Registration Page"
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    #Views signup API
-    #Take username and password
-    #Allow button to return to homepage
+
+    openfile = open('users.json')
+    user_database = json.load(openfile)
+    openfile.close()
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] in user_database:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template("error.html")
+        else:
+            user_database[request.form['username']] = request.form["password"]
+            json_object = json.dumps(user_database, indent = 4)
+            with open("users.json", "w") as outfile:
+                outfile.write(json_object)
+
+            return redirect(url_for('login'))
+
     return render_template("register.html")
 
 ##
@@ -36,9 +61,65 @@ def register():
 ## USER HOMEPAGE SPECIFIC FOR EACH USER
 ##
 ##
-@app.route('/user/<username>')
-def userhome(username):
-    return render_template("userhome.html")
+@app.route('/user')
+def userhome():
+    openfile = open('survey.json')
+    empty_survey = json.load(openfile)
+    openfile.close()
+    username = request.args['username']
+    return render_template("userhome.html", variable=empty_survey, username=username)
+
+##
+##  Company Pages
+##
+@app.route("/company_login", methods=['GET', 'POST'])
+def company_login():
+
+    openfile = open('companies.json')
+    company_database = json.load(openfile)
+    openfile.close()
+
+    error = None
+    if request.method == 'POST':
+        if company_database[request.form['username']] != request.form['password']:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template("error.html")
+        else:
+            return redirect(url_for('company_home', username=request.form['username']))
+
+    return render_template('company_login.html', error=error)
+
+#"Registration Page"
+@app.route("/company_register", methods=['GET', 'POST'])
+def company_register():
+
+    openfile = open('companies.json')
+    user_database = json.load(openfile)
+    openfile.close()
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] in user_database:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template("error.html")
+        else:
+            user_database[request.form['username']] = request.form["password"]
+            json_object = json.dumps(user_database, indent = 4)
+            with open("companies.json", "w") as outfile:
+                outfile.write(json_object)
+
+            return redirect(url_for('company_login'))
+
+    return render_template("company_register.html")
+
+
+@app.route('/company_home')
+def company_home():
+    username = request.args['username']
+    temp = [1,2,3,4,5,6,7,8,9]
+    return render_template("company_home.html", variable="1234", username=username, temp=temp)
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)

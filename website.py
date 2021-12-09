d7ca8af3e4cbf78e04829595e1f35948a04d4ab3
Mansi Patel <47717106+mansi-3006@users.noreply.github.com>
2021-12-08 19:20:30 -0500

    Update

--- website.py
+++ website.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import json
+import pymysql as pymysql
 
 
 #user_database = {"Jeremy":"pass","Bob":"corn"}
@@ -63,11 +64,55 @@ def register():
 ##
 @app.route('/user')
 def userhome():
+
     openfile = open('survey.json')
     empty_survey = json.load(openfile)
     openfile.close()
     username = request.args['username']
-    return render_template("userhome.html", variable=empty_survey, username=username)
+    #return render_template("userhome.html", variable=empty_survey, username=username)
+    return render_template("test_survey.html", variable=empty_survey, username=username)
+
+@app.route('/survey', methods=['GET','POST'])
+def survey():
+    connection=pymysql.connect(host='localhost', user='root', password='mansipatel', db='SOC')
+    # name=request.form['name']
+
+    age=request.form['age']
+    gender=request.form['gender']
+    country=request.form['country']
+    state=request.form['state']
+    selfemp=request.form['selfemp']
+    famhist=request.form['famhist']
+    treat=request.form['treat']
+    workinter=request.form['workinter']
+    noemp=request.form['noemp']
+    remote=request.form['remote']
+    techcomp=request.form['techcomp']
+    benefits=request.form['benefits'].replace("'","''")
+    care=request.form['care']
+    wellprog=request.form['wellprog'].replace("'","''")
+    help=request.form['help'].replace("'","''")
+    anonymity=request.form['anonymity'].replace("'","''")
+    leave=request.form['leave'].replace("'","''")
+    menconsequences=request.form['menconsequences']
+    phyconsequences=request.form['phyconsequences']
+    coworkers=request.form['coworkers']
+    supervisor=request.form['supervisor']
+    meninterview=request.form['meninterview']
+    phyinterview=request.form['phyinterview']
+    menvsphy=request.form['menvsphy'].replace("'","''")
+    obsconsq=request.form['obsconsq']
+    comments=request.form['comments']
+    print(comments)
+    cursor=connection.cursor()
+    # INSERT INTO survey (id,Age,Gender,Country,state,self_employed,family_history,treatment,work_interfere,no_employees,remote_work,tech_company,benefits,care_options,seek_help,anonymity,leave1,mental_health_consequence,phys_health_consequence,coworkers,supervisor,mental_health_interview,mental_vs_physical,obs_consequence,comments) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}');
+
+    cursor.execute("INSERT INTO survey(Age,Gender,Country,state,self_employed,family_history,treatment,work_interfere,no_employees,remote_work,tech_company,benefits,care_options,wellness_program,seek_help,anonymity,leave1,mental_health_consequence,phys_health_consequence,coworkers,supervisor,mental_health_interview,phys_health_interview,mental_vs_physical,obs_consequence,comments) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(age,gender,country,state,selfemp,famhist,treat,workinter,noemp,remote,techcomp,benefits,care,wellprog,help,anonymity,leave,menconsequences,phyconsequences,coworkers,supervisor,meninterview,phyinterview,menvsphy,obsconsq,comments))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return render_template("register.html")
+
 
 ##
 ##  Company Pages
@@ -120,6 +165,6 @@ def company_home():
     return render_template("company_home.html", variable="1234", username=username, temp=temp)
 
 
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
